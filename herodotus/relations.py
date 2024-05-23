2a7643b6b9fef6c6d5d3dbcf0cefb99eddb7612a
Archaversine <adam.brohl.w@gmail.com>
2024-05-23 10:16:17 -0500

    LLM rephraser

--- herodotus/relations.py
+++ herodotus/relations.py
@@ -1,4 +1,27 @@
+import os
+import openai
+
 import herodotus_fast as hero
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+# Python template used to reword relation outputs
+reword_template = PromptTemplate.from_template(
+    '''
+    Your job is to reword phrases. For example you might rewrite 
+
+    \"Bob walked before alice talked\" 
+
+    as \"Before Alice talked, Bob walked\"
+
+    Now, rephrase the following sentence: 
+
+    \"{sentence}\"
+
+    only state the rephrasing, nothing else.
+    '''
+)
 
 # Given two names, return a sentence where the first name commits an action 
 # that precedes the action committed by the second name.
@@ -37,14 +60,34 @@ def finished_by(tree: hero.GrammarTree, name1: str, name2: str) -> str:
 def equals(tree: hero.GrammarTree, name1: str, name2: str) -> str:
     return tree.generate_from_format(f"'{name1} 'V|1#part 'exactly 'when '{name2} V|2#part")
 
-if __name__ == '__main__':
-    hero.init_conjugation()
-    tree = hero.parse_file('verb-test.cfg')
+# Use ChatGPT to reword a sentence whilst preserving the temporal logic
+def reword(text: str) -> str:
+    prompt = reword_template.format(sentence=text)
+    result = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": prompt}
+        ]
+    )
 
-    for _ in range(5):
-        print(precedes(tree, 'Bob', 'Alice'))
+    return result["choices"][0]["message"]["content"]
+
+if __name__ == '__main__':
+    load_dotenv()
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+    #hero.init_conjugation()
+    #tree = hero.parse_file('verb-test.cfg')
+
+    #for _ in range(5):
+        #generated = precedes(tree, 'Bob', 'Alice')
+    generated = "Bob walked before Alice talked"
+    print(f"Generated: {generated}")
+    print(f"Reworded: {reword(generated)}")
 
     print('-' * 20)
 
-    for _ in range(5): 
-        print(meets(tree, 'Bob', 'Alice'))
+    #for _ in range(5): 
+        #generated = meets(tree, 'Bob', 'Alice')
+    generated = "Bob stops walking as Alice starts to talk"
+    print(f"Reworded: {reword(generated)}")
+    print(f"Generated: {generated}")
